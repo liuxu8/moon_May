fe482e325d2be91c0f579fb0f24051d4d9db3a56
liuxu8 <892243561@qq.com>
2020-03-19 18:28:54 +0800

    首次添加项目，希望不会报错

---
+++ src/main/java/com/miss/testng/suite/PayTest.java
@@ -0,0 +1,11 @@
+package com.miss.testng.suite;
+
+import org.testng.annotations.Test;
+
+public class PayTest {
+@Test
+    public void PaySuccess(){
+        System.out.println("确定支付200元？");
+    }
+
+}
